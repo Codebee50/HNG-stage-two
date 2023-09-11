a9a1fed4858f3e7f4910e216922778c003906a42
Code bee <codebee345@outlook.com>
2023-09-11 22:44:22 +0100

    modified serializers.py file

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Person
 from . import validators
 
-#serializes data according to the Person model, providing a means to validating incoming data
+#serializes data according to the Person model, adding a layer of validation and security 
 class PersonSerializer(serializers.ModelSerializer):
     name = serializers.CharField(validators = [validators.validate_unique_name])
     class Meta:
